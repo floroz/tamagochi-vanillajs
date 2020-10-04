500acdd4e6ce5dddd9bb0a101f214f2160320154
Daniele <tortora87@gmail.com>
2020-10-04 14:54:33 +0100

    fix binding

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,58 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
-    console.log("clock: ", this.clock);
     return this.clock;
   },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      return;
+    }
+
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+      default:
+        break;
+    }
+  },
+  startGame() {
+    console.log("hatchibg");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awoken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+  },
+  changeWeather() {
+    console.log("change-weather");
+  },
+  cleanUpPoop() {
+    console.log("poop");
+  },
+  feed() {
+    console.log("feed");
   },
 };
+
+gameState.handleUserAction = gameState.handleUserAction.bind(gameState);
 
 export default gameState;
